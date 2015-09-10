eef8637f2fd2c67c451bd2bff32cfe39293acc59
Carl Bai <carlbai@gmail.com>
2015-09-09 21:09:07 -0500

    Added barebone Transaction class and Firebase connection to MainActivity

--- app/src/main/java/com/baibye/cluster/MainActivity.java
+++ app/src/main/java/com/baibye/cluster/MainActivity.java
@@ -5,12 +5,35 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.ValueEventListener;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Firebase.setAndroidContext(this);
+
+        Firebase myFirebaseRef = new Firebase("https://resplendent-heat-4997.firebaseIO.com/");
+        myFirebaseRef.child("message").setValue("Do you have data? You'll love Firebase.");
+
+        myFirebaseRef.child("message").addValueEventListener(new ValueEventListener() {
+
+            @Override
+            public void onDataChange(DataSnapshot snapshot) {
+                System.out.println(snapshot.getValue());  //prints "Do you have data? You'll love Firebase."
+            }
+
+            @Override
+            public void onCancelled(FirebaseError error) {
+            }
+
+        });
     }
 
     @Override
---
+++ app/src/main/java/com/baibye/cluster/Transaction.java
@@ -0,0 +1,14 @@
+package com.baibye.cluster;
+
+import org.joda.time.DateTime;
+
+import java.math.BigDecimal;
+
+/**
+ * Created by carlbai on 8/27/15.
+ */
+public class Transaction {
+    private String description;
+    private DateTime date;
+    private BigDecimal amount;
+}
